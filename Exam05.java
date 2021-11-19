b835a491e1b743abc63dab467f1dcd6b99dfb89a
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-19 16:42:53 +0900

    for문

---
+++ Exam05.java
@@ -0,0 +1,30 @@
+
+public class Exam05 {
+
+	public static void main(String[] args) {
+		// for문 : 주로 반복 횟수가 정해진 경우에 사용.(횟수가 있는 >> ~언제부터 ~~ 언제까지)
+
+		// 1 을 5번 출력하시오.
+
+//		for (int i = 1; i <= 5; i++) { //i =1 이 1번, i<+5 가 2번, syso가 3번, i++ 가 4번, i<=5가 5번 syso가 6번 ...... // 마지막 i값은 6이된다.
+//			System.out.println(i);
+//		}
+
+//		// 11부터 30까지 출력하시오.
+//		for(int a = 11; a <=30; a++) {
+//			System.out.println(a);
+//		}
+		
+		//96에서 53까지 출력하시오.
+		for(int b = 96; b >=53; b--) {
+			System.out.println(b);
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
