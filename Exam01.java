113b70f8f98a9bb0b089e26da2f76957b0873a2d
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-19 16:33:01 +0900

    while조건문 실습

---
+++ Exam01.java
@@ -0,0 +1,34 @@
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		//whill을 사용해서 콘솔에 1부터 10까지 출력
+		
+		int a= 1;
+		
+		
+		while(a < 11) {
+			System.out.println(a);
+			a++;
+				}
+		System.out.println(a);
+		
+//		while(true) {
+//			System.out.println(a);
+//			a++;
+//			if(a > 10)
+//				break;
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Exam02.java
@@ -0,0 +1,45 @@
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		
+		// while을 활용하여 1부터 100까지의 숫자중 3의 배수만 출력하시오
+		
+		int num = 0;
+		
+//		while(num < 101) {
+//			System.out.println(num);
+//			num +=3;
+//		}
+//		
+//		while(true){
+//			System.out.println(num);
+//			num +=3;
+//			if(num > 101) {
+//				break;
+//			}
+//		}
+		
+		while(num < 101) {
+			num++;
+			if(num%3 == 0)
+				System.out.println(num);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
